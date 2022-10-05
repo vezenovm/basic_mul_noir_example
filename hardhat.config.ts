cd58ae868788e58a210d670e1b86a282029dec4c
vezenovm <mvezenov@gmail.com>
2022-10-05 03:17:27 +0000

    clean up config and readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,21 +9,6 @@ const config: HardhatUserConfig = {
       optimizer: { enabled: true, runs: 5000 },
     },
   },
-  // solidity: {
-  //   compilers: [
-  //       // verifier contract will get stack too deep error unless 0.6.10 and settings are specified
-  //       {
-  //           version: "0.6.10",
-  //           settings: {
-  //             evmVersion: 'istanbul',
-  //             optimizer: { enabled: true, runs: 200 },
-  //           },
-  //       },
-  //       {
-  //           version: "0.8.0"
-  //       }
-  //   ]
-  // },
   networks: {
     hardhat: {
       blockGasLimit: 10000000,
